d0d9ee9f151614a645e7ff5c9ed44cea1813d25f
Kamal Guliyev <kamalag@code.edu.az>
2020-12-27 22:47:42 +0400

    27dec up

---
+++ MySignalR/MySignalR/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Text;
+//using MySignalR.Models;
+
+namespace MySignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MySignalR/MySignalR/Models/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MySignalR.Models
+{
+    public class ChatHub : Hub
+    {
+        //public async Task Send(string nick, string message)
+        //{
+        //    await Clients.All.SendAsync("Send", nick, message);
+        //}
+
+        public async Task Send()
+        {
+            SqlConnection con = new SqlConnection("data source=.; database=MyDb; integrated security=SSPI");
+            SqlCommand cm = new SqlCommand("Select * from Cards", con);
+            con.Open();
+            SqlDataReader sdr = cm.ExecuteReader();
+            StringBuilder sb = new StringBuilder();
+            while (sdr.Read())
+            {
+                sb.Append(sdr["Title"]);
+            }
+
+            con.Close();
+
+            await Clients.All.SendAsync("Send", sb.ToString());
+        }
+    }
+}
